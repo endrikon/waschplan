63c74f5684baf349b6e2737245004893222b3331
Endrit <endrit.konjuhi@tiko.energy>
2025-12-20 12:19:44 +0100

    feat: clean html output

--- app/src-tauri/src/html.rs
+++ app/src-tauri/src/html.rs
@@ -3,17 +3,19 @@ use std::fs::File;
 use std::io::Write;
 
 use crate::types::{self, DayHTMLData, YearMap};
-use build_html::{self, Html, HtmlContainer, HtmlPage, Table, TableCell, TableCellType, TableRow};
+use build_html::{
+    self, Html, HtmlContainer, HtmlElement, HtmlPage, Table, TableCell, TableCellType, TableRow,
+};
 use datetime::Month;
 
 fn create_row(day_data: &DayHTMLData) -> TableRow {
-    let day = TableCell::default().with_raw(day_data.date.clone());
-    let date = TableCell::default().with_raw(day_data.day.clone());
+    let date = TableCell::default().with_raw(day_data.date.clone());
+    let day = TableCell::default().with_raw(day_data.day.clone());
     let appartment = TableCell::default().with_raw(day_data.appartment.clone());
     TableRow::new()
-        .with_cell(day)
-        .with_cell(date)
-        .with_cell(appartment)
+        .with_cell(date.with_attributes([("class", "fs-7 date")]))
+        .with_cell(day.with_attributes([("class", "fs-7 day")]))
+        .with_cell(appartment.with_attributes([("class", "fs-7 apartment")]))
 }
 
 fn create_month_table(day_data: &[DayHTMLData]) -> Table {
@@ -27,13 +29,27 @@ fn create_month_row(row_data: &[(&i8, &Vec<DayHTMLData>)]) -> TableRow {
 
     row_data.iter().fold(TableRow::new(), |row, (month, data)| {
         let current_month = Month::from_zero(**month).unwrap();
-        row.with_cell(TableCell::new(TableCellType::Data).with_table(
-            create_month_table(data).with_header_row(vec![types::month_to_string(current_month)]),
-        ))
+        row.with_cell(
+            TableCell::new(TableCellType::Data).with_table(
+                create_month_table(data)
+                    .with_attributes([("class", "table table-sm table-striped")])
+                    .with_custom_header_row(
+                        TableRow::new().with_cell(
+                            TableCell::new(TableCellType::Header)
+                                .with_raw(types::month_to_string(current_month))
+                                .with_attributes([("class", "fs-7")]),
+                        ),
+                    ),
+            ),
+        )
     })
 }
 
-pub fn create_year_html(config: &types::Config, year_map: &YearMap) -> Result<(), Box<dyn Error>> {
+pub fn create_year_html(
+    config: &types::Config,
+    year_map: &YearMap,
+    year: u16,
+) -> Result<(), Box<dyn Error>> {
     let path = format!("{}.html", config.title);
     let mut output = File::create(path)?;
     let YearMap(map) = year_map;
@@ -42,13 +58,63 @@ pub fn create_year_html(config: &types::Config, year_map: &YearMap) -> Result<()
     let month_triples: Vec<Vec<(&i8, &Vec<DayHTMLData>)>> =
         month_data_vec.chunks(3).map(|x| x.to_vec()).collect();
 
-    let table = month_triples.iter().fold(
-        Table::new().with_caption(config.title.clone()),
-        |table, triple| table.with_custom_body_row(create_month_row(triple)),
-    );
+    let table = month_triples
+        .iter()
+        .fold(Table::new(), |table, triple| {
+            table.with_custom_body_row(create_month_row(triple))
+        })
+        .with_attributes([("class", "table table-sm table-borderless")]);
     let page = HtmlPage::new()
-        .with_title(config.title.clone())
+        .with_html(
+            HtmlElement::new(build_html::HtmlTag::Heading5)
+                .with_child(
+                    HtmlElement::new(build_html::HtmlTag::Span)
+                        .with_attribute("id", "house-name")
+                        .with_raw(config.title.clone())
+                        .into(),
+                )
+                .with_child(
+                    HtmlElement::new(build_html::HtmlTag::Span)
+                        .with_attribute("id", "year")
+                        .with_raw(year)
+                        .into(),
+                ),
+        )
+        .with_style(
+            r#"
+            @page {
+                margin-top: 0.75cm;
+            }
+            .fs-7 {
+                font-size: 0.6rem;
+                padding-top: 0px !important;
+                padding-bottom: 0px !important;
+            }
+            .date {
+                width: 10%;
+                padding-right: 0cm !important;
+                font-weight: bold;
+            }
+            .day {
+                width: 20%;
+                padding-right: 0cm !important;
+            }
+            .apartment {
+                width: 70%;
+            }
+            #house-name {
+                float: left;
+            }
+            #year {
+                float: right;
+            }"#,
+        )
+        .with_head_link(
+            "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css",
+            "stylesheet",
+        )
         .with_table(table)
         .to_html_string();
+
     write!(output, "{}", page).map_err(|err| Box::new(err) as _)
 }
